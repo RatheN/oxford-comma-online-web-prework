72aa42736389179a6a70caaf71f19c4dd906b3dc
Nathan Rathe <nprathe10@gmail.com>
2019-05-31 03:44:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,8 @@ def oxford_comma(array)
     array.join
   elsif array.size == 2
     array.join(" and ")
-  elsif array.size > 2 && array.size < 4
-    array[0] << ", "
-    array[1] << ", and "
-    array.join
-  elsif array.size > 3
-    array[-1].insert(0, "and")
+  elsif array.size > 2
+    array[-1].insert(0, "and ")
     array.join(", ")
   end
 end
