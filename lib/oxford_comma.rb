4ff5f702bbec7ea895f8c356dcb306051975c61e
Nathan Rathe <nprathe10@gmail.com>
2019-05-31 02:47:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.size < 2
+    array.join
+  elsif array.size > 1 && array.size < 3
+    array.join(" and ")
+  elsif array.size > 2 && array.size < 4
+    array[0] << ", "
+    array[1] << ", and "
+    array.join
+  elsif array.size > 3
 
+
+  end
 end
