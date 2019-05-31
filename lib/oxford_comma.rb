07416d1e51909b41a2bf7cd6362387e3a7acbc6d
Nathan Rathe <nprathe10@gmail.com>
2019-05-31 03:43:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 def oxford_comma(array)
   if array.size < 2
     array.join
-  elsif array.size > 1 && array.size < 3
+  elsif array.size == 2
     array.join(" and ")
   elsif array.size > 2 && array.size < 4
     array[0] << ", "
     array[1] << ", and "
     array.join
   elsif array.size > 3
-    array[-1] << (0, "and")
+    array[-1].insert(0, "and")
     array.join(", ")
   end
 end
